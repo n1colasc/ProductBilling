0ac28eca634ac597bbfd61a076e3699081a7370a
n1colasc <nicolascastiblancob@gmail.com>
2024-05-25 11:54:02 -0500

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductBilling.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductBilling.WebApiApplication" Language="C#" %>
